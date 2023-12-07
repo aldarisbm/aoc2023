a7c2f188286890f8e4bb3e8fa8ef8784722334cb
Jose Berrio <aldarisbm@gmail.com>
2023-12-07 09:03:58 -0500

    Catching up day 5 part one

--- day_five.go
+++ day_five.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 func dayFivePartOne() {
 	data := loadData("five")
-	seeds := strings.Split(strings.Split(strings.Split(data, "\n")[0], ": ")[1], " ")
+	seedsString := strings.Split(strings.Split(strings.Split(data, "\n")[0], ": ")[1], " ")
+	seeds := make([]int, len(seedsString))
+	for i, v := range seedsString {
+		n, _ := strconv.Atoi(v)
+		seeds[i] = n
+	}
+
 	seedToSoil := getMapping(seeds, data, "seed-to-soil map:\n")
-	fmt.Println(seedToSoil)
-	//soilToFertilizer := getMapping(seeds, data, "soil-to-fertilizer map:\n")
-	//fertilizerToWater := getMapping(seeds, data, "fertilizer-to-water map:\n")
-	//waterToLight := getMapping(seeds, data, "water-to-light map:\n")
-	//lightToTemperature := getMapping(seeds, data, "light-to-temperature map:\n")
-	//temperatureToHumidity := getMapping(seeds, data, "temperature-to-humidity map:\n")
-	//humidityToLocation :=  getMapping(seeds, data, "humidity-to-location map:\n")
+	soilToFertilizer := getMapping(getValues(seedToSoil), data, "soil-to-fertilizer map:\n")
+	fertilizerToWater := getMapping(getValues(soilToFertilizer), data, "fertilizer-to-water map:\n")
+	waterToLight := getMapping(getValues(fertilizerToWater), data, "water-to-light map:\n")
+	lightToTemperature := getMapping(getValues(waterToLight), data, "light-to-temperature map:\n")
+	temperatureToHumidity := getMapping(getValues(lightToTemperature), data, "temperature-to-humidity map:\n")
+	humidityToLocation := getMapping(getValues(temperatureToHumidity), data, "humidity-to-location map:\n")
+
+	minRes := slices.Min(getValues(humidityToLocation))
+	fmt.Printf("Day Five Part One Result: %d\n", minRes)
+}
+
+func dayFivePartTwo() {
+	_ = loadData("five")
 }
 
 func getMatrix(d, sep string) [][]int {
@@ -35,53 +47,35 @@ func getMatrix(d, sep string) [][]int {
 	return matrix
 }
 
-func getMapping(seeds []string, data string, sep string) *sync.Map {
-	s := make([]int, len(seeds))
-	for i, v := range seeds {
-		n, _ := strconv.Atoi(v)
-		s[i] = n
-	}
+func getMapping(seeds []int, data string, sep string) map[int]int {
+	mappings := make(map[int]int)
 	m := getMatrix(data, sep)
 
-	mapping := &sync.Map{}
-
-	for _, seed := range s {
-		for _, r := range m {
-			destStart := r[0]
-			rangeLen := r[2]
-			if seed-destStart > 0 && seed-destStart < rangeLen {
-				mapping.Store(seed, seed+destStart)
+	for _, seed := range seeds {
+		for _, row := range m {
+			destStart := row[0]
+			sourceStart := row[1]
+			length := row[2]
+			if seed >= sourceStart && seed < sourceStart+length {
+				mappings[seed] = destStart + (seed - sourceStart)
 			}
 		}
 	}
 
-	mapping.Range(func(key, value any) bool {
-		fmt.Printf("k %d, v %d", key, value)
-		return true
-	})
-	//50 98 2
-	// dest start 50
-	// source start 98
-	// range length 2
-	// 98 99 == 50 51
-	// 97 - 98 = -1
-	// num - range start > 0 < rang length = good
-
-	//52 50 48
-	// seed
-	//The first line has a destination range start of 50, a source range start of 98, and a range length of 2.
-	//This line means that the source range starts at 98 and contains two values: 98 and 99.
-	//The destination range is the same length, but it starts at 50, so its two values are 50 and 51.
-	//With this information, you know that seed number 98 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
-	for _, row := range m {
-		destinationStart := row[0]
-		sourceStart := row[1]
-		rangeLen := row[2]
-		for i := 0; i < rangeLen; i++ {
-			if intInSlice(sourceStart, s) {
-				mapping.Store(sourceStart+1, destinationStart+1)
-			}
+	for _, seed := range seeds {
+		if _, ok := mappings[seed]; !ok {
+			mappings[seed] = seed
 		}
 	}
-	return mapping
+	return mappings
+}
+
+func getValues(m map[int]int) []int {
+	values := make([]int, len(m))
+	i := 0
+	for _, v := range m {
+		values[i] = v
+		i++
+	}
+	return values
 }
